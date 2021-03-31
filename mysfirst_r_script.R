042141493e7ea0ebb8369cac7ff6f4c6c358ec2f
letspairup <jatin.pinal@gmail.com>
2021-03-31 19:32:00 -0400

    Create mysfirst_r_script.R

---
+++ mysfirst_r_script.R
@@ -0,0 +1,8 @@
+example <- matrix(c(1,2,3,4,5,6,7,8),ncol = 4,nrow = 2, byrow = TRUE)
+unique(iris$Species)
+install.packages("ggplot2")
+library(ggplot2)
+install.packages("devtools")
+help("KernSmooth")
+??KernSmooth
+library(KernSmooth)
